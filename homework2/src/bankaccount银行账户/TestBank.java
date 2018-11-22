295bb060bec2eb7d5929815c755d3921376f26e7
kingdas <das_jin@163.com>
2018-11-22 10:13:26 +0800

    .

--- homework2/src/bankaccount银行账户/TestBank.java
+++
@@ -1,9 +0,0 @@
-package bankaccountÒøĞĞÕË»§;
-
-public class TestBank {
-	public static void main(String[] args) {
-		Account a = new SavingAccount(12345, 10000, "121212", 0.05);
-		System.out.println(a.getId() + " " + a.getBalance() + " " + a.getPassword() + " "
-		+((SavingAccount)a).getInterestRate());
-	}
-}
